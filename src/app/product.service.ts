b97f00d09c09f3d4ab0cec0537e54d02a5b05975
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2019-08-07 11:42:07 +0200

    read me changed, service added

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,11 @@ export class ProductService {
 
   constructor() { }
 
-  searchProduct(maxPrice: number): Product[] {
-    return [];
+  searchProduct(maxPrice: number): Observable <Product[]> {
+    return of([
+      new Product(1, 'cahier', 2.5, 'grand cahier', 'lightblue'),
+      new Product(2, 'stylo', 1.99, 'stylo bleu', 'green'),
+      new Product(3, 'sac', 14, 'sac de sport')
+    ]);
   }
 }
