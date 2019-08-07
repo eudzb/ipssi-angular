8e9bf411d5d13136195d1f8721008195afc72565
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2019-08-07 11:20:01 +0200

    first commit

---
+++ src/app/product.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+
+  searchProduct(maxPrice: number): Product[] {
+    return [];
+  }
+}
